327b875da49da7f3a48888da5680309b6611eac6
arakiyo <arakiyo@gmail.com>
2024-07-30 14:17:03 +0000

    vuetify

---
+++ chico/pages/chico/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-card width="400">
+      <v-card-item>
+        <v-card-title>This is a CHICO {{ pageId }} PAGE title</v-card-title>
+
+        <v-card-subtitle>This is a subtitle</v-card-subtitle>
+      </v-card-item>
+
+      <v-card-text> This is content </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const pageId = Number(route.params.id);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "CHICO SITE",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+    },
+  },
+  css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  srcDir: "chico",
+});
